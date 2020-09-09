564f60668997c6f8015bc6498385e402a6ab90df
Vladimir Shameev <vladimir.shameev@gmail.com>
2020-09-09 10:31:41 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,18 +1,19 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-var cors = require('cors')
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const cors = require('cors')
 
-var indexRouter = require('./routes/index');
-var geolocationRouter = require('./routes/geolocation');
+const indexRouter = require('./routes/index');
+const geolocationRouter = require('./routes/geolocation');
 
-var app = express();
+const app = express();
 
 app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+// Why do you need cookieParser there ?
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
